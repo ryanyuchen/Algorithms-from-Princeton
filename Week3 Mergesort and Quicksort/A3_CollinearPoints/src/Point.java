eb1edf472919651115178807ca20129ad80ae886
ryanyuchen <vanchen10@gmail.com>
2022-01-14 16:53:08 -0500

    add Point.java and LineSegment.java

--- Week3 Mergesort and Quicksort/A3_CollinearPoints/src/Point.java
+++ Week3 Mergesort and Quicksort/A3_CollinearPoints/src/Point.java
@@ -1,4 +1,10 @@
+/*
+Project Link: https://coursera.cs.princeton.edu/algs4/assignments/collinear/specification.php
+Author: Yu Chen
+ */
+
 import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
 
 import java.util.Comparator;
 
@@ -51,6 +57,10 @@ public class Point implements Comparable<Point> {
      */
     public double slopeTo(Point that) {
         /* YOUR CODE HERE */
+        if (this.x == that.x && this.y == that.y) return Double.NEGATIVE_INFINITY;
+        else if (this.x == that.x) return Double.POSITIVE_INFINITY;
+        else if (this.y == that.y) return +0.0;
+        else return (double) (that.y - this.y) / (that.x - this.x);
     }
 
     /**
@@ -67,6 +77,11 @@ public class Point implements Comparable<Point> {
      */
     public int compareTo(Point that) {
         /* YOUR CODE HERE */
+        if (this.y < that.y) return -1;
+        if (this.y > that.y) return 1;
+        if (this.x < that.x) return -1;
+        if (this.x > that.x) return 1;
+        return 0;
     }
 
     /**
@@ -77,8 +92,16 @@ public class Point implements Comparable<Point> {
      */
     public Comparator<Point> slopeOrder() {
         /* YOUR CODE HERE */
+        return new SlopeOrderComparator();
     }
 
+    private class SlopeOrderComparator implements Comparator<Point> {
+        public int compare(Point p1, Point p2) {
+            if (slopeTo(p1) < slopeTo((p2))) return -1;
+            else if (slopeTo(p1) > slopeTo(p2)) return 1;
+            else return 0;
+        }
+    }
 
     /**
      * Returns a string representation of this point.
@@ -97,5 +120,11 @@ public class Point implements Comparable<Point> {
      */
     public static void main(String[] args) {
         /* YOUR CODE HERE */
+        Point p0 = new Point(0, 0);
+        Point p1 = new Point(5, 2);
+        Point p2 = new Point(2, 1);
+        StdOut.print(p0.compareTo(p1));
+        StdOut.print(p1.slopeTo(p2));
+
     }
 }
