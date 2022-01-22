1601a12a2863bf6abdd21def87ca910f12f163e9
ryanyuchen <vanchen10@gmail.com>
2022-01-22 10:21:17 -0500

    A4 1st commit

---
+++ Week4 Priority Queues and Elementary Symbol Tables/A4_8Puzzle/src/Solver.java
@@ -0,0 +1,23 @@
+/*
+Project Link: https://coursera.cs.princeton.edu/algs4/assignments/8puzzle/specification.php
+Author: Yu Chen
+ */
+
+public class Solver {
+
+    // find a solution to the initial board (using the A* algorithm)
+    public Solver(Board initial)
+
+    // is the initial board solvable? (see below)
+    public boolean isSolvable()
+
+    // min number of moves to solve initial board; -1 if unsolvable
+    public int moves()
+
+    // sequence of boards in a shortest solution; null if unsolvable
+    public Iterable<Board> solution()
+
+    // test client (see below)
+    public static void main(String[] args)
+
+}
