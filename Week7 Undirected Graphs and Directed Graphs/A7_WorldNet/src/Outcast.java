af24d9e376b45c8989916fc93aadd41d687907b8
ryanyuchen <vanchen10@gmail.com>
2022-02-09 15:01:15 -0500

    worldnet 1st commit

---
+++ Week7 Undirected Graphs and Directed Graphs/A7_WorldNet/src/Outcast.java
@@ -0,0 +1,20 @@
+/*
+Project Link: https://coursera.cs.princeton.edu/algs4/assignments/wordnet/specification.php
+Author: Yu Chen
+ */
+
+
+public class Outcast {
+    // constructor takes a WordNet object
+    public Outcast(WordNet wordnet) {
+
+    }
+    
+    // given an array of WordNet nouns, return an outcast
+    public String outcast(String[] nouns) {
+
+    }
+
+    // see test client below
+    public static void main(String[] args)
+}
