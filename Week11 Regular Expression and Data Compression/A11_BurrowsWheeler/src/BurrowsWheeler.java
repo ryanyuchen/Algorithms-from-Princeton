609fa57daf029ee7a7300d5ea74d76d0b03a3155
ryanyuchen <vanchen10@gmail.com>
2022-03-09 17:29:56 -0500

    burrowswheeler 1st commit

---
+++ Week11 Regular Expression and Data Compression/A11_BurrowsWheeler/src/BurrowsWheeler.java
@@ -0,0 +1,20 @@
+/*
+Project Link: https://coursera.cs.princeton.edu/algs4/assignments/burrows/specification.php
+Author: Yu Chen
+*/
+
+public class BurrowsWheeler {
+
+    // apply Burrows-Wheeler transform,
+    // reading from standard input and writing to standard output
+    public static void transform()
+
+    // apply Burrows-Wheeler inverse transform,
+    // reading from standard input and writing to standard output
+    public static void inverseTransform()
+
+    // if args[0] is "-", apply Burrows-Wheeler transform
+    // if args[0] is "+", apply Burrows-Wheeler inverse transform
+    public static void main(String[] args)
+
+}
---
+++ Week11 Regular Expression and Data Compression/A11_BurrowsWheeler/src/CircularSuffixArray.java
@@ -0,0 +1,20 @@
+/*
+Project Link: https://coursera.cs.princeton.edu/algs4/assignments/burrows/specification.php
+Author: Yu Chen
+*/
+
+public class CircularSuffixArray {
+
+    // circular suffix array of s
+    public CircularSuffixArray(String s)
+
+    // length of s
+    public int length()
+
+    // returns index of ith sorted suffix
+    public int index(int i)
+
+    // unit testing (required)
+    public static void main(String[] args)
+
+}
