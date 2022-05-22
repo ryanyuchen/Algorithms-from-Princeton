75d10288d2ea5e6c94683f596ad77b483935700a
ryanyuchen <vanchen10@gmail.com>
2022-05-22 09:10:59 -0400

    add boggle

--- Week10 Tries and Substring Search/A10_Boggle/src/BoggleSolver.java
+++ Week10 Tries and Substring Search/A10_Boggle/src/BoggleSolver.java
@@ -4,6 +4,8 @@ Author: Yu Chen
 */
 
 
+import edu.princeton.cs.algs4.SET;
+
 public class BoggleSolver {
 
     private final Node root;
@@ -18,18 +20,118 @@ public class BoggleSolver {
     // Initializes the data structure using the given array of strings as the dictionary.
     // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
     public BoggleSolver(String[] dictionary) {
+        if (dictionary == null) {
+            throw new IllegalArgumentException("argument is null");
+        }
+        root = new Node();
 
+        for (String s : dictionary) {
+            createNode(s);
+        }
+    }
 
+    private void createNode(String s) {
+        Node x = root;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (x.next[c - 'A'] == null) {
+                x.next[c - 'A'] = new Node();
+            }
+            x = x.next[c - 'A'];
+        }
+        x.value = s.length();
     }
 
     // Returns the set of all valid words in the given Boggle board, as an Iterable.
     public Iterable<String> getAllValidWords(BoggleBoard board) {
+        if (board == null) {
+            throw new IllegalArgumentException("argument is null");
+        }
+        int rows = board.rows();
+        int cols = board.cols();
+        SET<String> validWords = new SET<>();
+        boolean[][] visited = new boolean[rows][cols];
 
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                dfs(board, root, i, j, "", validWords, visited);
+            }
+        }
+        return validWords;
+    }
+
+    private void dfs(BoggleBoard board, Node x, int i, int j, String word, SET<String> validWords, boolean[][] visited) {
+        char c = board.getLetter(i, j);
+        if (x == null || x.next[c - 'A'] == null) {
+            return;
+        }
+        String s;
+        if (c == 'Q') {
+            s = word + "QU";
+            x = x.next['Q' - 'A'];
+            if (x.next['U' - 'A'] == null) {
+                return;
+            }
+            x = x.next['U' - 'A'];
+        } else {
+            s = word + c;
+            x = x.next[c - 'A'];
+        }
+
+        if (s.length() >= 3 && x.value != -1) {
+            validWords.add(s);
+        }
+        visited[i][j] = true;
+        int m = board.rows();
+        int n = board.cols();
+        for (int p = 0; p < rowLabel.length; p++) {
+            int xi = i + rowLabel[p];
+            int yj = j + colLabel[p];
+            if (xi < 0 || xi >= m || yj < 0 || yj >= n || visited[xi][yj]) {
+                continue;
+            }
+            dfs(board, x, xi, yj, s, validWords, visited);
+        }
+        visited[i][j] = false;
     }
 
     // Returns the score of the given word if it is in the dictionary, zero otherwise.
     // (You can assume the word contains only the uppercase letters A through Z.)
     public int scoreOf(String word) {
-        
+        if (word == null) {
+            return 0;
+        }
+        if (contains(word)) {
+            int length = word.length();
+            if (length <= 2) {
+                return 0;
+            }
+            if (length == 3 || length == 4) {
+                return 1;
+            }
+            if (length == 5) {
+                return 2;
+            }
+            if (length == 6) {
+                return 3;
+            }
+            if (length == 7) {
+                return 5;
+            }
+            return 11;
+        }
+        return 0;
+    }
+    
+    private boolean contains(String s) {
+        Node x = root;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (x.next[c - 'A'] == null) {
+                return false;
+            }
+            x = x.next[c - 'A'];
+        }
+        return x.value != -1;
     }
 }
